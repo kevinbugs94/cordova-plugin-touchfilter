e2cfa6785e6842fc7c4562a1ba8994797efd97bf
kevinbugs94 <66101919+kevinbugs94@users.noreply.github.com>
2026-04-30 18:05:50 -0500

    Update TouchFilter.js
    test 0.2

--- www/TouchFilter.js
+++ www/TouchFilter.js
@@ -1,7 +1,9 @@
 var exec = require('cordova/exec');
-exports.enableFilter = function(success, error) {
-   exec(success, error, "TouchFilter", "enableFilter", []);
+
+var TouchFilter = {
+enable: function(success, error) {
+exec(success, error, "TouchFilter", "enable", []);
+}
 };
-exports.isOverlayEnabled = function(success, error) {
-   exec(success, error, "TouchFilter", "isOverlayEnabled", []);
-};
+
+module.exports = TouchFilter;
