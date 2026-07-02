5f08bfcff82b2954b0acf797ca1933a73d08daab
kevinbugs94 <66101919+kevinbugs94@users.noreply.github.com>
2026-07-02 18:54:41 -0500

    Update TouchFilter.js
    version 2.0

--- www/TouchFilter.js
+++ www/TouchFilter.js
@@ -1,9 +1,80 @@
 var exec = require('cordova/exec');
 
+var SERVICE = "TouchFilter";
+
 var TouchFilter = {
-enable: function(success, error) {
-exec(success, error, "TouchFilter", "enable", []);
-}
+
+    /**
+     * Habilita la protección contra tapjacking.
+     */
+    enable: function(success, error) {
+
+        exec(
+            success || function(){},
+            error || function(){},
+            SERVICE,
+            "enable",
+            []
+        );
+
+    },
+
+    /**
+     * Deshabilita la protección.
+     * (Preparado para futuras versiones Java)
+     */
+    disable: function(success, error) {
+
+        exec(
+            success || function(){},
+            error || function(){},
+            SERVICE,
+            "disable",
+            []
+        );
+
+    },
+
+    /**
+     * Devuelve si la protección está habilitada.
+     */
+    isEnabled: function(success, error) {
+
+        exec(
+            success || function(){},
+            error || function(){},
+            SERVICE,
+            "isEnabled",
+            []
+        );
+
+    },
+
+    /**
+     * Obtiene estadísticas del plugin.
+     */
+    getStatistics: function(success, error) {
+
+        exec(
+            success || function(){},
+            error || function(){},
+            SERVICE,
+            "getStatistics",
+            []
+        );
+
+    }
+
 };
+
+
+// Registro global
+if (window.cordova) {
+
+    window.cordova.plugins = window.cordova.plugins || {};
+
+    window.cordova.plugins.TouchFilter = TouchFilter;
+
+}
 
 module.exports = TouchFilter;
